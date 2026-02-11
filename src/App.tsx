5f2bfa0f58349746c09cfda00f43f450bbc27283
wissem <med.wissem.rouine@gmail.com>
2026-02-11 11:37:26 +0100

    remove global loading and make FilmsDeHammamLifEvent lazy

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ import { MetadataProvider } from "./lib/metadata/metadata-provider";
 import ScrollToTopOnLocationChange from "./lib/router/scroll-to-top";
 import NotFound from "./pages/NotFound";
 import PageLoader from "./pages/PageLoader";
-import FilmsDeHammamLifEvent from "./pages/events/FilmsDeHammamLifEvent";
 
 const Home = lazy(() => import("./pages/Home"));
 const Movies = lazy(() => import("./pages/Movies"));
 const MoviePlayer = lazy(() => import("./pages/MoviePlayer"));
 const Events = lazy(() => import("./pages/Events"));
 const YdourEvent = lazy(() => import("./pages/events/YdourEvent"));
+const FilmsDeHammamLifEvent = lazy(() => import("./pages/events/FilmsDeHammamLifEvent"));
 const Blog = lazy(() => import("./pages/Blog"));
 const About = lazy(() => import("./pages/About"));
 const Palmares = lazy(() => import("./pages/Palmares"));
@@ -28,36 +28,34 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Suspense fallback={<PageLoader />}>
-      <ThemeProvider>
-        {/* <TooltipProvider>
+    <ThemeProvider>
+      {/* <TooltipProvider>
         <Toaster />
         <Sonner /> */}
-        <MetadataProvider>
-          <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
-            <ScrollToTopOnLocationChange />
-            <Layout>
-              <Suspense fallback={<PageLoader />}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/movies/:id" element={<MoviePlayer />} />
-                  <Route path="/movies" element={<Movies />} />
-                  <Route path="/events/ydour" element={<YdourEvent />} />
-                  <Route path="/events/films-de-hammamlif" element={<FilmsDeHammamLifEvent />} />
-                  <Route path="/events" element={<Events />} />
-                  <Route path="/blog" element={<Blog />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/palmares" element={<Palmares />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </Suspense>
-            </Layout>
-          </BrowserRouter>
-        </MetadataProvider>
-        {/* </TooltipProvider> */}
-      </ThemeProvider>
-    </Suspense>
+      <MetadataProvider>
+        <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
+          <ScrollToTopOnLocationChange />
+          <Layout>
+            <Suspense fallback={<PageLoader />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies/:id" element={<MoviePlayer />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/events/ydour" element={<YdourEvent />} />
+                <Route path="/events/films-de-hammamlif" element={<FilmsDeHammamLifEvent />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/palmares" element={<Palmares />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
+          </Layout>
+        </BrowserRouter>
+      </MetadataProvider>
+      {/* </TooltipProvider> */}
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
