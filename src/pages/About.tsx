6edb92fd1518e5b240a33d3919afcddf0d27fe09
Zineddine-mlaouhi <Zineddine.mlaouhi@outlook.com>
2026-02-11 18:39:41 +0100

    added soura mta3 mahdi

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -216,7 +216,7 @@ export default function About() {
       name: "Mehdi Ben Farhat",
       role: "Member",
       bio: "",
-      image: ""
+      image: `${BASE}Members/mahdi.jpg`
     },
     {
       name: "Youssef El Behi",
